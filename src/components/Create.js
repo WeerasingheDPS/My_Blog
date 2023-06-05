007022cec1e80dbaa869983031d38b2674dd33f8
unknown <2019cs186@stu.ucsc.lk>
2023-06-05 21:44:13 +0530

    added blog title area

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title,setTitle]=useState('');
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
             <form action="">
                 <lable>Blog Title:</lable>
-                <input type="text"  required/>
+                <input type="text"
+                  required
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  />
                 <lable>Blog Body:</lable>
                 <textarea required> </textarea>
                 <lable>Blog Author:</lable>
@@ -13,6 +21,7 @@ const Create = () => {
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
             </form>
         </div>
       );
